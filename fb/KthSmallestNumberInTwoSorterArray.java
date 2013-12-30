fddf1188cc2ced818f5e923dc81891218260b547
pankaj4u4m <pankaj4u4m@gmail.com>
2013-12-30 22:33:36 +0530

    http://oj.leetcode.com/problems/median-of-two-sorted-arrays/

--- fb/KthSmallestNumberInTwoSorterArray.java
+++ fb/KthSmallestNumberInTwoSorterArray.java
@@ -2,73 +2,30 @@ package fb;
 
 public class KthSmallestNumberInTwoSorterArray {
 
-    public static Integer kthSmallest(int a[], int b[], int k) {
-        int n = a.length;
-        int m = b.length;
-        if (m + n < k)
-            return null;
-        if (n + m == k)
-            return Math.max(a[n - 1], b[m - 1]);
-        if (n > m) {
-            int t[] = a;
-            a = b;
-            b = t;
-            int c = n;
-            n = m;
-            m = c;
-        }
-
-        int i = Math.min(n, k / 2);
-        int j = k - i;
-        int steps = (i + 1) / 2;
-        int times = 2;
-
-        while (times > 0) {
-            System.out.println(steps + " = " + i + " : " + j);
-            if (a[i - 1] > b[j - 1]) {
-                i -= steps;
-                j += steps;
+    public static Integer kthSmallest(int[] A, int[] B, int i, int j, int n, int m, int k) {
+        if (n - i > m - j) {
+            return kthSmallest(B, A, j, i, m, n, k);
+        } else if (n == i) {
+            return B[k-1];
+        } else if (k == 1) {
+            return Math.min(A[i], B[j]);
+        } else {
+            int x = Math.min(n - i, k / 2);
+            int y = k - x;
+            ;
+            if (A[i + x - 1] <= B[j + y - 1]) {
+                return kthSmallest(A, B, i + x, j, n, m, k - x);
             } else {
-                i += steps;
-                j -= steps;
+                return kthSmallest(A, B, i, j + y, n, m, k - y);
             }
-            if (steps == 1) {
-                times--;
-            }
-
-            steps = (steps + 1) / 2;
         }
-        System.out.println(steps + " = " + i + " : " + j);
-
-        int x = medianOf3(a, b, i - 1, j - 1);
-        System.out.println(x);
-        return x;
     }
 
-    private static Integer medianOf3(int[] a, int[] b, int i, int j) {
-        List<Integer> list = new ArrayList<>();
-        if (i - 1 >= 0) {
-            list.add(a[i - 1]);
-        }
-        list.add(a[i]);
-        if (i + 1 < a.length) {
-            list.add(a[i + 1]);
-        }
-        if (j - 1 >= 0) {
-            list.add(b[j - 1]);
-        }
-        list.add(b[j]);
-        if (j + 1 < b.length) {
-            list.add(b[j + 1]);
-        }
-        Collections.sort(list);
-        return list.get(list.size() / 2);
-    }
 
 	public static void main(String[] args) {
 		int a[] = new int[] { 1, 2, 3, 4, 5, 6, 7 };
 		int b[] = new int[] { };
 		System.out
-				.println(kthSmallest(a, b, 6 ));
+				.println(kthSmallest(a, b, 0, 0, a.length, b.length, 6 ));
 	}
 }
